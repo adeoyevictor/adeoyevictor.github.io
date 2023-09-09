b256b1e600a05b387f35663f05ee0e30b5704b67
adeoyevictor <adeoyevictor16@gmail.com>
2023-09-09 18:44:12 +0100

    animation

--- js/index.js
+++ js/index.js
@@ -1,12 +1,45 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".service", {
+  duration: 1.5,
+  opacity: 0,
+  y: "random(-400, 400)",
+  stagger: 0.25,
+  ease: "back",
+  scale: 0.3,
+  scrollTrigger: {
+    trigger: ".service",
+    toggleActions: "restart pause resume pause",
+    // start: "top center",
+    end: "bottom 100px",
+  },
+  //   repeat: 1
+});
+gsap.from(".portfolio .portfolio__item", {
+  duration: 1.5,
+  opacity: 0,
+  y: "random(-400, 400)",
+  stagger: 0.25,
+  scale: 0.3,
+  ease: "back",
+  scrollTrigger: {
+    trigger: ".portfolio .portfolio__item",
+    toggleActions: "restart pause resume pause",
+    // start: "top center",
+    end: "bottom 100px",
+  },
+  //   repeat: 3
+});
