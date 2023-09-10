2dd26c281c46d9010907d3c68549d604a95f2c13
adeoyevictor <adeoyevictor16@gmail.com>
2023-09-10 11:57:54 +0100

    favicon added

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ navLinks.forEach((link) => {
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".services .service", {
-  duration: 1,
+  duration: 0.5,
   opacity: 0,
   y: "random(-200, 200)",
   stagger: 0.25,
@@ -22,7 +22,7 @@ gsap.from(".services .service", {
   scale: 0.3,
   scrollTrigger: {
     trigger: ".services",
-    toggleActions: "play pause resume pause",
+    toggleActions: "play pause restart restart",
   },
 });
 
@@ -35,10 +35,65 @@ gsap.from(".portfolio .portfolio__item", {
   ease: "back",
   scrollTrigger: {
     trigger: ".portfolio",
-    toggleActions: "play pause restart pause",
+    toggleActions: "play pause resume restart",
   },
 });
 
 // play pause resume reverse restart reset complete none
 // 1 enter screen
 // 2 when it goes forward past endpoint
+
+gsap.from(".my__name", {
+  duration: 2,
+  y: 300,
+  ease: "elastic",
+  scrollTrigger: {
+    trigger: ".my__name",
+    toggleActions: "play pause restart restart",
+  },
+});
+gsap.from(".section__title--about", {
+  duration: 2,
+  y: 300,
+  ease: "elastic",
+  scrollTrigger: {
+    trigger: ".section__title--about",
+    toggleActions: "play pause restart restart",
+  },
+});
+
+gsap.from(".section__subtitle--intro", {
+  duration: 2,
+  y: -300,
+  ease: "bounce",
+  scrollTrigger: {
+    trigger: ".section__subtitle--intro",
+    toggleActions: "play pause restart restart",
+  },
+});
+
+gsap.set(".intro__img", {
+  transformOrigin: "50% 50%",
+});
+
+gsap.to(".intro__img", {
+  duration: 2,
+  rotation: 360,
+  scrollTrigger: {
+    trigger: ".intro__img",
+    toggleActions: "play pause restart restart",
+  },
+});
+
+gsap.set(".about-me__img", {
+  transformOrigin: "50% 50%",
+});
+
+gsap.to(".about-me__img", {
+  duration: 2,
+  rotation: 360,
+  scrollTrigger: {
+    trigger: ".about-me__img",
+    toggleActions: "play pause restart restart",
+  },
+});
